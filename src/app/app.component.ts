36333b5f0d3b4170bebf1859831a54d8d925f317
Donna <donna@duel.tech>
2025-01-03 15:02:11 +0000

    in-bundle languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,57 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import {
   TranslateDirective,
   TranslatePipe,
   TranslateService,
 } from "@ngx-translate/core";
+import deDE from "../../public/i18n/de-DE.json";
+import enGB from "../../public/i18n/en-GB.json";
+import enUSVictoriasSecret from "../../public/i18n/en-US-victoriassecret.json";
+import enUS from "../../public/i18n/en-US.json";
+import deDERabanne from "../../public/i18n/partial/de-DE-rabanne.json";
+import enUSRabanne from "../../public/i18n/partial/en-US-rabanne.json";
 
 @Component({
+  standalone: true,
   selector: "app-root",
-  imports: [CommonModule, TranslatePipe, TranslateDirective],
+  imports: [CommonModule, FormsModule, TranslatePipe, TranslateDirective],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  private readonly translate = inject(TranslateService);
+  protected readonly translate = inject(TranslateService);
 
   protected two = this.translate.stream("app.two");
+  protected name = "Carla";
+  protected time = 1234;
+  protected welcome = this.translate.stream("app.welcome", { name: this.name });
+  protected copyrightYear = 1999;
 
   constructor() {
-    this.translate.addLangs(["de", "en"]);
-    this.translate.setDefaultLang("en");
-    this.translate.use("en");
+    this.translate.addLangs([
+      "de-DE",
+      "en-GB",
+      "en-US",
+      "en-US-victoriassecret",
+      "de-DE-rabanne",
+      "en-US-rabanne",
+    ]);
+
+    this.translate.setTranslation("de-DE", deDE);
+    this.translate.setTranslation("en-GB", enGB);
+    this.translate.setTranslation("en-US", enUS);
+    this.translate.setTranslation("en-US-victoriassecret", enUSVictoriasSecret);
+    this.translate.setTranslation("en-US-rabanne", enUSRabanne);
+    this.translate.setTranslation("de-DE-rabanne", deDERabanne);
+
+    this.translate.setDefaultLang("en-US");
+
+    this.translate.use("en-US");
+  }
+
+  changeLanguage(language: string): void {
+    this.translate.use(language);
   }
 }
