b617767b21515c117c58d3669a0abd0a71b741fa
Donna <donna@duel.tech>
2025-01-03 11:26:58 +0000

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import {
+  TranslateDirective,
+  TranslatePipe,
+  TranslateService,
+} from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [CommonModule, TranslatePipe, TranslateDirective],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'ngx-translate';
+  private readonly translate = inject(TranslateService);
+
+  protected two = this.translate.stream("app.two");
+
+  constructor() {
+    this.translate.addLangs(["de", "en"]);
+    this.translate.setDefaultLang("en");
+    this.translate.use("en");
+  }
 }
